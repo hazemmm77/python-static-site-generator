38b4b3a3b130d7e3e6ec4c73719b44f1f9416425
Hazem Fathy <hazemmm77773@gmail.com>
2020-09-29 16:38:45 +0200

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -16,7 +16,7 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                self.run_parse(path)
+                self.run_parser(path)
 
     def load_parser(self,extension):
          for parser in self.parsers:
