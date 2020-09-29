bfe9432ee2dc6b95423eceb8fb6cd3573f5db667
Hazem Fathy <hazemmm77773@gmail.com>
2020-09-29 15:54:39 +0200

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -24,5 +24,5 @@ class Parser:
 
 class ResourceParser(Parser):
     extensions=[".jpg",".png",".gif",".css",".html"]
-          def parse(self,path:Path,source:Path,dest:Path):
+    def parse(self,path:Path,source:Path,dest:Path):
               base.copy(path,source,dest)
