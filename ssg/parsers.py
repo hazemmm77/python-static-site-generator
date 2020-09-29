4cf18c2c90c0d410c76563f7a8fafb4007105e91
Hazem Fathy <hazemmm77773@gmail.com>
2020-09-29 15:09:07 +0200

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -4,10 +4,7 @@ import shutil
 class Parser:
     extensions:List[str]=[]
     def valid_extension(self,extension):
-        if extension in self.extensions:
-            return True
-        else:
-            return False
+        return extension in self.extensions:
 
     def parse(path,source,dest):
         raise NotImplementedError
