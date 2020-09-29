1abaa038f2239d3d5274153a75fb50b46c366b17
Hazem Fathy <hazemmm77773@gmail.com>
2020-09-29 14:52:46 +0200

    Update parsers.py

---
+++ ssg/parsers.py
@@ -0,0 +1,26 @@
+from typing import List
+from pathlib import Path
+import shutil
+class Parser:
+    extensions=List()
+    def valid_extension(self,extension):
+        if extension in self.extensions:
+            return True
+        else:
+            return False
+
+    def parse(Path path, Path source,Path dest):
+        raise NotImplementedError
+
+    def read(path):
+         # using with statement
+         with open(path, 'r') as file:
+	     return file.read()
+
+    def write(path,dest,content,ext=".html"):
+        full_path=dest /path.with_suffix(ext).name
+         with open(path, 'w') as file:
+             file.write(content)
+
+    def copy(self,path,source,dest):
+        shutil.copy2(path, dest/ path.relative_to(source)) 
