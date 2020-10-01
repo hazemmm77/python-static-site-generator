a87b8b5d9d380d91b35be570b296255a2ef2b460
Hazem Fathy <hazemmm77773@gmail.com>
2020-10-01 16:02:00 +0200

    Update content.py

---
+++ ssg/content.py
@@ -0,0 +1,11 @@
+import re
+from yaml import load,FullLoader
+from collections.abc import Mapping
+class Content(Mapping):
+    __delimeter="^(?:-|\+){3}\s*$"
+    __regex=re.compile(__delimeter,re.MULTILINE)
+
+    def load(self,cls,string):
+        _, fm, content=self.__regex.split(string,2)
+        base.load(fm,Loader= FullLoader)
+        return cls(metadata,content) 
